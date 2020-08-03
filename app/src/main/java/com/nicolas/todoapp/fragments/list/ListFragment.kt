38e95f8fab936b3e8167fafb5e4088894977414f
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-02 23:41:21 -0400

    menus created and added to each view

--- app/src/main/java/com/nicolas/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/nicolas/todoapp/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.nicolas.todoapp.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.nicolas.todoapp.R
 import kotlinx.android.synthetic.main.fragment_list.view.*
@@ -28,7 +26,14 @@ class ListFragment : Fragment() {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
 
+        // Set menu
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 
