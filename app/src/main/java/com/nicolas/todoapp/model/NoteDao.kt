f47edc6e8740d04b7dc172216af8a03988aab0f9
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-03 01:14:52 -0400

    Note dao and note database implemented

--- app/src/main/java/com/nicolas/todoapp/model/NoteDao.kt
+++ app/src/main/java/com/nicolas/todoapp/model/NoteDao.kt
@@ -1,4 +1,17 @@
 package com.nicolas.todoapp.model
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
 interface NoteDao {
+
+    @Query("SELECT * FROM note_table ORDER BY id ASC")
+    fun getAllData(): LiveData<List<Note>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertNote(note: Note)
 }
