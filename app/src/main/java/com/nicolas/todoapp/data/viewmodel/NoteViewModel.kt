5b3e6a1955c2d1540d6bef756fc6b441cdcbbe7a
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-03 02:01:51 -0400

    viewmodel added

---
+++ app/src/main/java/com/nicolas/todoapp/data/viewmodel/NoteViewModel.kt
@@ -0,0 +1,33 @@
+package com.nicolas.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.nicolas.todoapp.data.NoteDatabase
+import com.nicolas.todoapp.data.model.Note
+import com.nicolas.todoapp.data.repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(application: Application) : AndroidViewModel(application){
+
+    private val noteDao = NoteDatabase.getDatabase(
+        application
+    ).noteDao()
+    private val repository: NoteRepository
+
+    private val getAllNotes: LiveData<List<Note>>
+
+    init {
+        repository = NoteRepository(noteDao)
+        getAllNotes = repository.getAllData
+    }
+
+    fun insertNote(note: Note) {
+        viewModelScope.launch ( Dispatchers.IO ) {
+            repository.insertNote(note)
+        }
+    }
+
+}
