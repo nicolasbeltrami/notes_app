f97ed207aa98461b7389c8980fcdd354bbcb4374
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-04 19:30:28 -0400

    model marked as parcelable

--- app/src/main/java/com/nicolas/todoapp/data/model/Note.kt
+++ app/src/main/java/com/nicolas/todoapp/data/model/Note.kt
@@ -1,14 +1,17 @@
 package com.nicolas.todoapp.data.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.nicolas.todoapp.data.model.Priority
+import kotlinx.android.parcel.Parcelize
 
 @Entity(tableName = "note_table")
+@Parcelize
 data class Note(
     @PrimaryKey(autoGenerate = true)
     var id: Int,
     var title: String,
     var priority: Priority,
     var description: String
-)
+): Parcelable
