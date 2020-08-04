924b3c4797a6eb3e570be9245776ae30d98b4d7c
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-04 01:40:52 -0400

    recycler view and adapter implemented on list fragment

--- app/src/main/java/com/nicolas/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/nicolas/todoapp/fragments/list/ListFragment.kt
@@ -3,13 +3,19 @@ package com.nicolas.todoapp.fragments.list
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nicolas.todoapp.R
+import com.nicolas.todoapp.data.viewmodel.NoteViewModel
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 
 class ListFragment : Fragment() {
 
+    private val noteViewModel: NoteViewModel by viewModels()
+    private val adapter: ListAdapter by lazy { ListAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,14 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_list, container, false)
+
+        val recyclerView = view.rvList
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+        noteViewModel.getAllNotes.observe(viewLifecycleOwner, Observer {data ->
+            adapter.setData(data)
+        })
 
         view.fabAddTask.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
