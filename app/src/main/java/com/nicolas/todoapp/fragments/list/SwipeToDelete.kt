443f75b3876eddbac2275a6476a5b28855df9ff1
nicolasbeltrami <nicolas.beltrami@gmail.com>
2020-08-05 01:44:17 -0400

    swipe to delete helper class

--- app/src/main/java/com/nicolas/todoapp/fragments/list/SwipeToDelete.kt
+++ app/src/main/java/com/nicolas/todoapp/fragments/list/SwipeToDelete.kt
@@ -1,4 +1,17 @@
 package com.nicolas.todoapp.fragments.list
 
-class SwipeToDelete {
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class SwipeToDelete: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+
+    // This class is created to not fill with code the fragment, we just use the onSwiped method on the List Fragment
+
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
 }
